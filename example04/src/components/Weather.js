8ff2e16891f87a37e9f038be0c4b6dffc4d5a2b9
Mr.chu <chuhongkyu@gmail.com>
2023-10-31 12:50:04 +0900

    Add framer-motion

--- example04/src/components/Weather.js
+++ example04/src/components/Weather.js
@@ -1,6 +1,7 @@
 import { useLoader } from "@react-three/fiber";
 import { useMemo } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { motion } from "framer-motion-3d";
 
 const Weather = (props) => {
     const { position, rotation, weather } = props;
@@ -17,9 +18,9 @@ const Weather = (props) => {
     }, [weather])
 
     return(
-        <mesh position={position} rotation={rotation}>
+        <motion.mesh whileHover={{scale:1.5, transition:{duration:0.5}}} position={position} rotation={rotation}>
             <primitive object={weatherModel} />
-        </mesh>
+        </motion.mesh>
     )
 }
 
